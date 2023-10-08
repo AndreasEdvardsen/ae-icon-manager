8602c14c0375ad5c7312a6a85b2b58d84979b538
Andreas Edvardsen <edvardsenandreas@outlook.com>
2023-10-08 08:37:41 +0200

    fix url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -153,7 +153,7 @@ function filterIcons(icons) {
 
 function getDistinctIconPrefixes() {
   axios
-    .get("http://localhost:4000/distinctIconPrefixes")
+    .get("https://seashell-app-wwgas.ondigitalocean.app/distinctIconPrefixes")
     .then(function (response) {
       prefixes.value = response.data;
     });
@@ -162,7 +162,9 @@ function getDistinctIconPrefixes() {
 function getIcons(prefix) {
   loading.value = true;
   axios
-    .get(`http://localhost:4000/icons?prefix=${prefix.value}`)
+    .get(
+      `https://seashell-app-wwgas.ondigitalocean.app/icons?prefix=${prefix.value}`
+    )
     .then(function (response) {
       allicons.value = response.data;
       loading.value = false;
